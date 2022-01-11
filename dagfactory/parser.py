a656e38cd6e4272150e2895b350eea9e24e0266a
JoaoJanini <jjaninir@gmail.com>
2022-01-11 15:37:07 -0300

    added current time function

--- dagfactory/parser.py
+++ dagfactory/parser.py
@@ -3,8 +3,15 @@ from jinja2 import Template
 import yaml
 from airflow.hooks.base import BaseHook
 from json import loads
+from datetime import datetime
 
 DEFAULT_NOT_SPECIFIED = 'DEFAULT_NOT_SPECIFIED'
+
+
+def get_current_date():
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    return today_date
+
 
 class Parser:
     def render(self, filePath, extra_vars={}):
@@ -14,7 +21,9 @@ class Parser:
                     'var': self.var,
                     'conn': self.conn,
                     'extra_vars': extra_vars,
-                    'json_loads': loads
+                    'json_loads': loads,
+                    'get_current_date': get_current_date
+
                 })
                 print(template)
                 return yaml.safe_load(template)
@@ -23,7 +32,7 @@ class Parser:
 
     def var(self, var_name, default=DEFAULT_NOT_SPECIFIED):
         # Using this hack for allowing specifyin None as default
-        # and still break if no default is specified and the var 
+        # and still break if no default is specified and the var
         # is not defined at airflow
         if default == DEFAULT_NOT_SPECIFIED:
             return Variable.get(var_name)
